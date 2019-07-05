51477515a0c91ca3b5bb2854aa0414a5edf5d7d9
almaginet <almaginet@gmail.com>
2019-07-05 16:46:38 -0500

    new

--- web/forms.py
+++ web/forms.py
@@ -16,30 +16,30 @@ class Bookingform(forms.Form):
 
 	item = forms.ModelChoiceField(queryset=Item.objects.all(),label="Select a Tour",required=True)
 	date = forms.DateField(
-        label='Date of Tour', 
+        label='¿Qué dia prefiere agendar?', 
         widget=forms.TextInput(attrs={'class':'form-control'})
         )
 	tickets = forms.CharField(
-        label='Number of Persons', 
+        label='¿Cuantas personas vienen con usted?', 
         widget=forms.NumberInput(attrs={'class':'form-control'})
         )
 	first = forms.CharField(
-        label='Your First Name', 
+        label='¿Cual es su nombre?', 
         widget=forms.TextInput(attrs={'class':'form-control'})
         )
 	last = forms.CharField(
-        label='Your Last Name', 
+        label='Cual es su Apellido?', 
         widget=forms.TextInput(attrs={'class':'form-control'})
         )
 	phone = forms.CharField(
-        label='Your Phone Number', 
+        label='Indiquenos un numero de contacto', 
         widget=forms.TextInput(attrs={'class':'form-control'})
         )
 	email = forms.EmailField(
-        label='Your Email', 
+        label='Indiquenos un Email', 
         widget=forms.EmailInput(attrs={'class':'form-control'})
         )
 	message = forms.CharField(
-        label='Aditional Message', 
+        label='Por favor escriba un mensaje adicional', 
         widget=forms.Textarea(attrs={'class':'form-control'})
         )
