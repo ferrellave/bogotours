2ba2eb0c65c318cffc95b8639402998573e4df72
almaginet <almaginet@gmail.com>
2019-07-28 16:32:21 -0500

    new

--- web/views.py
+++ web/views.py
@@ -42,16 +42,6 @@ def home(request):
     profiles = Profile.objects.all().order_by('-id')
     profilesn = Profile.objects.all().order_by('-id')
     client_ip, is_routable = get_client_ip(request)
-    if client_ip is None:
-    # Unable to get the client's IP address
-    else:
-    # We got the client's IP address
-        if is_routable:
-         # The client's IP address is publicly routable on the Internet
-        else:
-         # The client's IP address is private
-
-    # Order of precedence is (Public, Private, Loopback, None)
     return render_to_response(template,locals(),
                                 context_instance=RequestContext(request))
 
